4ebae3cba8e33ab5b2cff6cdda82b3d1fb9c30be
Tuan Hoang <nguyenledly1997@gmail.com>
2021-07-08 11:39:30 +0700

    update: transparent DGT

--- src/views/CityScopeJS/DeckglMap/deckglLayers/GeojsonLayer.js
+++ src/views/CityScopeJS/DeckglMap/deckglLayers/GeojsonLayer.js
@@ -11,7 +11,7 @@ export default function GeojsonLayer({ data }) {
     filled: true,
     extruded: true,
     lineWidthScale: 1,
-    getFillColor: (d) => {return hexToRgb(d.properties.fill)},
+    getFillColor: (d) => {return d.properties.TypeCode == "DGT" ? [0, 0, 0, 0] : hexToRgb(d.properties.fill)},
     getLineColor: (d) => hexToRgb(d.properties.stroke),
     lineWidthMinPixels: 2,
     getElevation: (d) => d.properties.height,
